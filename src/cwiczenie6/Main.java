ac4a4abb097381582e58ca2cddc5a7818fe7e869
MC140180 <140180@student.uwm.edu.pl>
2024-03-03 21:43:05 +0100

    solved 6 of 6

--- src/cwiczenie6/Main.java
+++ src/cwiczenie6/Main.java
@@ -2,15 +2,6 @@ package cwiczenie6;
 
 public class Main {
     public static void main(String[] args) {
-        Pracownik prac = new Pracownik("mechanik");
-
-        prac.setDojezdzac(new Rower());
-        prac.setSpedzanieWolnegoCzasu(new Silownia());
-        prac.setPracowac(new NaprawaSamochodow());
-
-
-        prac.jedzDoPracy();
-        prac.wykonujPrace();
-        prac.odpoczywaj();
+        Pracownik pracownik = new Pracownik("mechanik");
     }
 }
