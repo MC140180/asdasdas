1604c9d5125fcc793e50969dfec5581483e7a8ad
MC140180 <140180@student.uwm.edu.pl>
2024-03-09 16:58:33 +0100

    commit

--- src/cwiczenie3/Main.java
+++ src/cwiczenie3/Main.java
@@ -2,12 +2,12 @@ package cwiczenie3;
 
 public class Main {
     public static void main(String[] args) {
-        CarInfo carInfo = new SkodaInfo();
+        ICarInfo carInfo = new SkodaInfo();
         IMaxSpeed carSpeed = new NormalCarSpeed();
         Car car = new Car(carInfo, carSpeed);
 
 
-        car.getCarInfo().getName();
-        car.getMaxCarSpeed().getMaxSpeed();
+        System.out.println(car.getCarInfo().getName());
+        System.out.println(car.getMaxCarSpeed().getMaxSpeed());
     }
 }
